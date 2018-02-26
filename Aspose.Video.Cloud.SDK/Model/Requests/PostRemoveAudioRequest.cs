3f4410276dd23e544e8400c5689a3d3aaf6c956e
Pavel-Teplitsky <pavel.teplitsky@groupdocs.com>
2018-02-26 12:02:15 +0200

    Revert "SDK regenerated by CI server"
    This reverts commit bfec8bf336ef0fa4b41880100b9ffa09d65b37f4.

--- Aspose.Video.Cloud.SDK/Model/Requests/PostRemoveAudioRequest.cs
+++ Aspose.Video.Cloud.SDK/Model/Requests/PostRemoveAudioRequest.cs
@@ -24,12 +24,13 @@
 // --------------------------------------------------------------------------------------------------------------------
 namespace Aspose.Video.Cloud.Sdk.Model.Requests 
 {
-  using Aspose.Video.Cloud.Sdk.Model; 
+    using Aspose.Video.Cloud.Sdk.Model;
+    using System.Collections.Generic;
 
-  /// <summary>
-  /// Request model for <see cref="Aspose.Video.Cloud.Sdk.Api.VideoApi.PostRemoveAudio" /> operation.
-  /// </summary>  
-  public class PostRemoveAudioRequest : ICanModifyVideoFileRequest, IVideoFileRequest   
+    /// <summary>
+    /// Request model for <see cref="Aspose.Video.Cloud.Sdk.Api.VideoApi.PostRemoveAudio" /> operation.
+    /// </summary>  
+    public class PostRemoveAudioRequest : ICanModifyVideoFileRequest, IVideoFileRequest   
   {
         /// <summary>
         /// Initializes a new instance of the <see cref="PostRemoveAudioRequest"/> class.
